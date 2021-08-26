2088ddaabf2e7ac2399a96d13cddbe7f7b24420f
Kaushik Donthi <kaushikdr@gmail.com>
2021-08-25 22:53:13 -0400

    reduced size of GetTestFeeHistoryOracle

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.FeeHistory.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.FeeHistory.cs
@@ -35,7 +35,7 @@ namespace Nethermind.JsonRpc.Test.Modules.Eth
     {
         [TestCase("2", "earliest", "[0,10.5,20,60,90]", "\"result\":{\"oldestBlock\":\"0x0\",\"reward\":[[\"0x1\",\"0x1\",\"0x1\",\"0x2\",\"0x2\"]],\"baseFeePerGas\":[\"0x2\",\"0x2\"],\"gasUsedRatio\":[0.7]}")]
         [TestCase("1", "latest", "[0,10.5,20,60,90]", "\"result\":{\"oldestBlock\":\"0x1\",\"reward\":[[\"0x0\",\"0x0\",\"0x0\",\"0x0\",\"0x3\"]],\"baseFeePerGas\":[\"0x3\",\"0x3\"],\"gasUsedRatio\":[0.5]}")]
-        [TestCase("1", "pending", "[0,10.5,20,60,90]", "\"error\":{\"code\":-32002,\"message\":\"newestBlock: Block is not available\"}")]
+        [TestCase("1", "pending", "[0,10.5,20,60,90]", "\"result\":{\"oldestBlock\":\"0x1\",\"reward\":[[\"0x0\",\"0x0\",\"0x0\",\"0x0\",\"0x3\"]],\"baseFeePerGas\":[\"0x3\",\"0x3\"],\"gasUsedRatio\":[0.5]}")]
         [TestCase("2", "0x01", "[0,10.5,20,60,90]", "\"result\":{\"oldestBlock\":\"0x0\",\"reward\":[[\"0x1\",\"0x1\",\"0x1\",\"0x2\",\"0x2\"],[\"0x0\",\"0x0\",\"0x0\",\"0x0\",\"0x3\"]],\"baseFeePerGas\":[\"0x2\",\"0x3\",\"0x3\"],\"gasUsedRatio\":[0.7,0.5]}")]
         public async Task Eth_feeHistory(string blockCount, string blockParameter, string rewardPercentiles,
             string expectedResult)
@@ -54,60 +54,44 @@ namespace Nethermind.JsonRpc.Test.Modules.Eth
             // Gas Used and Reward: [(2,1), (5,2)]
             // Percentile Thresholds: [0, 0, 1, 4, 6]
             // Percentile Rewards: [1, 1, 1, 2, 2]
-            Transaction tx1SecondBlock = Build.A.Transaction.WithMaxPriorityFeePerGas(5).WithMaxFeePerGas(6).WithType(TxType.EIP1559).TestObject; //Reward: Min (6 - 3, 5) => 3
-            Transaction tx2SecondBlock = Build.A.Transaction.WithMaxPriorityFeePerGas(2).WithMaxFeePerGas(2).WithType(TxType.EIP1559).TestObject; //Reward: BaseFee (3) > FeeCap (2) => 0
+            
+            Transaction tx1SecondBlock = Build.A.Transaction.WithMaxPriorityFeePerGas(5).WithMaxFeePerGas(6)
+                .WithType(TxType.EIP1559).TestObject; //Reward: Min (6 - 3, 5) => 3
+            Transaction tx2SecondBlock = Build.A.Transaction.WithMaxPriorityFeePerGas(2).WithMaxFeePerGas(2)
+                .WithType(TxType.EIP1559).TestObject; //Reward: BaseFee (3) > FeeCap (2) => 0
             // Gas Used and Reward: [(3,0), (2,3)]
             // Percentile Thresholds: [0, 0, 1, 3, 4]
             // Percentile Rewards: [0, 0, 0, 0, 3]
-            Block firstBlock = Build.A.Block.
-                Genesis.
-                WithBaseFeePerGas(2).
-                WithGasUsed(7). //Todo fix gas Used
-                WithGasLimit(10).
-                WithTransactions(tx1FirstBlock, tx2FirstBlock).
-                TestObject;
-            Block secondBlock = Build.A.Block.
-                WithNumber(1).
-                WithBaseFeePerGas(3).
-                WithGasUsed(5).
-                WithGasLimit(10).
-                WithTransactions(tx1SecondBlock, tx2SecondBlock).
-                TestObject;
             
-            IBlockFinder blockFinder = Substitute.For<IBlockFinder>();
-            BlockParameter newestBlockParameter = new(1) {RequireCanonical = true};
-            blockFinder.FindBlock(newestBlockParameter).Returns(secondBlock);
-            blockFinder.FindParent(secondBlock, BlockTreeLookupOptions.RequireCanonical).Returns(firstBlock);
-            BlockParameter blockParameterEarliest = new(BlockParameterType.Earliest) {RequireCanonical = true};
-            BlockParameter blockParameterLatest = new(BlockParameterType.Latest) {RequireCanonical = true};
-            BlockParameter blockParameterPending = new(BlockParameterType.Pending) {RequireCanonical = true};
-            blockFinder.FindBlock(blockParameterEarliest).Returns(firstBlock);
-            blockFinder.FindBlock(blockParameterLatest).Returns(secondBlock);
-            blockFinder.FindBlock(blockParameterPending).Returns((Block?) null);
+            Block firstBlock = Build.A.Block.Genesis.WithBaseFeePerGas(2).WithGasUsed(7).WithGasLimit(10)
+                .WithTransactions(tx1FirstBlock, tx2FirstBlock).TestObject;
+            Block secondBlock = Build.A.Block.WithParentHash(firstBlock.Hash!).WithNumber(1).WithBaseFeePerGas(3)
+                .WithGasUsed(5).WithGasLimit(10).WithTransactions(tx1SecondBlock, tx2SecondBlock).TestObject;
+
+            BlockTree blockTree = Build.A.BlockTree().WithBlocks(firstBlock, secondBlock).TestObject;
             
             IReceiptStorage receiptStorage = Substitute.For<IReceiptStorage>();
-            receiptStorage.Get(firstBlock).Returns(new TxReceipt[]
-            {
-                new() {GasUsed = 2},
-                new() {GasUsed = 5}
-            });
-            receiptStorage.Get(secondBlock).Returns(new TxReceipt[]
-            {
-                new() {GasUsed = 2},
-                new() {GasUsed = 3}
-            });
+            receiptStorage.Get(firstBlock).Returns(new TxReceipt[] {new() {GasUsed = 2}, new() {GasUsed = 5}});
+            receiptStorage.Get(secondBlock).Returns(new TxReceipt[] {new() {GasUsed = 2}, new() {GasUsed = 3}});
+            
+            ISpecProvider specProvider = GetTestSpecProvider();
 
-            IReleaseSpec eip1559EnabledReleaseSpec = Substitute.For<IReleaseSpec>();
-            eip1559EnabledReleaseSpec.IsEip1559Enabled.Returns(true);
-            IReleaseSpec eip1559NotEnabledReleaseSpec = Substitute.For<IReleaseSpec>();
-            eip1559NotEnabledReleaseSpec.IsEip1559Enabled.Returns(false);
-            ISpecProvider specProvider = Substitute.For<ISpecProvider>();
-            specProvider.GetSpec(Arg.Is<long>(l => l == 2)).Returns(eip1559EnabledReleaseSpec);
-            specProvider.GetSpec(Arg.Is<long>(l => l == 1)).Returns(eip1559NotEnabledReleaseSpec);
             FeeHistoryOracle feeHistoryOracle =
-                GetSubstitutedFeeHistoryOracle(blockFinder: blockFinder, receiptStorage: receiptStorage, specProvider: specProvider);
+                GetSubstitutedFeeHistoryOracle(blockTree, receiptStorage, specProvider);
 
             return feeHistoryOracle;
+        }
+
+        private static ISpecProvider GetTestSpecProvider()
+        {
+            IReleaseSpec eip1559Enabled = Substitute.For<IReleaseSpec>();
+            eip1559Enabled.IsEip1559Enabled.Returns(true);
+            IReleaseSpec eip1559NotEnabled = Substitute.For<IReleaseSpec>();
+            eip1559NotEnabled.IsEip1559Enabled.Returns(false);
+            ISpecProvider specProvider = Substitute.For<ISpecProvider>();
+            specProvider.GetSpec(Arg.Is<long>(l => l == 2)).Returns(eip1559Enabled);
+            specProvider.GetSpec(Arg.Is<long>(l => l == 1)).Returns(eip1559NotEnabled);
+            return specProvider;
         }
     }
 }
